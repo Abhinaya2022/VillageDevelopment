b6b7203370271c8ec53cfe6ecaf504e0a4c54349
Trainee 2022 <trainee2022@audreeinfotech.com>
2022-11-29 11:09:58 +0530

    FIrst Commit of Village

---
+++ Village.Repository/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Village.Repository
+{
+    public class Class1
+    {
+    }
+}
---
+++ VillageDevelopment/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VillageDevelopment.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
